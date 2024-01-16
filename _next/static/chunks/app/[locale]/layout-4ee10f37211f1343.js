5bcecf2adfe3570ca7d0381ee5d18ca02745f1b2
Budakov Dmitry <63675683+BudakovDmitry@users.noreply.github.com>
2024-01-16 23:27:41 +0200

    Add files via upload

---
+++ _next/static/chunks/app/[locale]/layout-4ee10f37211f1343.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[203],{3523:function(n,e,t){Promise.resolve().then(t.t.bind(t,2445,23)),Promise.resolve().then(t.t.bind(t,4621,23))},2445:function(){},4621:function(){}},function(n){n.O(0,[267,971,938,744],function(){return n(n.s=3523)}),_N_E=n.O()}]);
